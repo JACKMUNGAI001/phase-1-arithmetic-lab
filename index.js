4dfd0878736565e128d05dff6803c8442d5c8a30
Jackson <jacksonmungai001@gmail.com>
2025-06-13 20:39:21 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 
 // Write your code here
+// 1. Create a variable called `num1` and set it equal to 31
+const num1 = 31;
+
+// 2. Create a variable called `num2` and set it equal to 2
+const num2 = 2;
+
+// 3. Create a variable called `multiply` that multiplies `num1` and `num2`
+const multiply = num1 * num2;
+
+// 4. Create a variable called `random` that generates a random integer greater than 0
+const random = Math.floor(Math.random() * 100) + 1;
+
+// 5. Create a variable called `num3` and set it equal to 24
+const num3 = 24;
+
+// 6. Create a variable called `num4` and set it equal to 5
+const num4 = 5;
+
+// 7. Create a variable called `mod` that calculates the remainder of dividing `num3` by `num4`
+const mod = num3 % num4;
+
+// 8. Create a variable called `max` that finds the highest number in a set
+const max = Math.max(1, 3, 5, 20, 10);
